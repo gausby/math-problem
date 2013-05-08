d32afdbe202a8ddd8ad01ee6637334d14d05ce0c
Martin Gausby <martin@gausby.dk>
2013-05-08 13:48:00 +0200

    Removed comma first style from the repl file.

--- repl.js
+++ repl.js
@@ -1,6 +1,6 @@
 /* global require */
-var repl = require('repl')
-,   solve = require('./lib/solve')
+var repl = require('repl'),
+    solve = require('./lib/solve')
 ;
 
 var local = repl.start('> ');
